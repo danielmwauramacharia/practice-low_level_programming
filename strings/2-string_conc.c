a874d151eef356db3a847c17ffdc0daaa7152ce4
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-01-18 06:09:19 -0800

    A code that concatenate string

---
+++ strings/2-string_conc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+  *main -a program that concatenated two strings
+  *
+  *Return: 0 success
+  */
+int main(void)
+{
+	char fname[30];
+	char lname[10];
+	int i = 0, j = 0, count1 = 0, count2 = 0, len1, len2, k=0;
+
+	printf("Enter your first name: \n");
+	scanf("%s", fname);
+	printf("Enter your second name: \n");
+	scanf("%s", lname);
+	while (fname[i] != '\0')
+{
+		count1++;
+		i++;
+}
+	len1 = count1;
+	printf("%d\n", len1);
+	while (lname[k] != '\0')
+{
+		count2++;
+		k++;
+}
+	len2 = count2;
+	printf("%d\n", len2);
+	for (j = 0; j <= len2; j++)
+{
+		fname[len1 + j] = lname[j];
+}
+	printf("Your names are: %s\n", fname);
+	return (0);
+}
