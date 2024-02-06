2774f348d7151e6e25db93f0832a75780ceab4cc
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-06 00:59:01 -0800

    prints a quote with some special characters

---
+++ intro_c/quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main -entry point of program
+  *
+  *Return: 1 for success
+  */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (!0);
+}
