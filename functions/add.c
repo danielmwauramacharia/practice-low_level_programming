2f1b39d46e6720117805a4f40386f37f55d3a499
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-09 06:43:07 -0800

    adds two integer and returns sum

---
+++ functions/add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *add - adds two numbers and return the sum
+  *@a: the first integer
+  *@b: second integer
+  *
+  *Return: sum
+  */
+int add(int a, int b)
+{
+	int sum = 0;
+
+	sum = a + b;
+	return (sum);
+}
