97caca0bd99f04b57aabdae467f06d594c5fc9c1
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-11 16:01:07 -0800

    function that prints number upto 98

---
+++ functions/print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_to_98 - prints from n to 98
+  *@n: the starting integer
+  *
+  *Return: return n
+  */
+void print_to_98(int n)
+{
+	int i, last = 98;
+
+	if (n == 98)
+		printf("%d\n", n);
+	else 
+{
+		if (n > 98)
+{
+			for (i = n; i > 98; i--)
+				printf("%d, ", i);
+}
+		else
+{
+			for (i = n; i < 98; i++)
+				printf("%d, ", i);
+
+}
+		printf("%d\n", last);
+}
+}
