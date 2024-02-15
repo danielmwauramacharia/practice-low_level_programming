92a3662f7068f2ebeee92aabced2f69447f7703f
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-15 05:10:07 -0800

    function that prints a diagonal line using \

---
+++ functions/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *print_diagonal -prints a diagonal line using \
+ *@n: the size of the line
+ */
+void print_diagonal(int n)
+{
+	int i = 0, j;
+	
+	if (!(n == 0 || n < 0))
+		while (i < n)
+{
+			if (i == 0)
+{
+				_putchar('\\');
+				_putchar('\n');
+}
+			else 
+{
+				j = 0;
+				while (j < i)
+{
+					_putchar(' ');
+					j++;
+}
+				_putchar('\\');
+				_putchar('\n');
+}
+			i++;
+}	else
+		_putchar('\n');
+}
