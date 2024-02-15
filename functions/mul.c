9213eecdf86d95836610d05ce20c4a36e4bbdac6
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-02-15 00:44:30 -0800

    function that returns multiplication of two integers

---
+++ functions/mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  *mul -a function that multiplies two integers
+  *@a: the first integer
+  *@b: the second integer
+  *
+  *Return: mul
+  */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
